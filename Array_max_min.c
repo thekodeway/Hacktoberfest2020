d7fff7d091de514e85b77cff5301a9fc0ec9f528
thekodeway <61931801+thekodeway@users.noreply.github.com>
2020-10-02 09:38:49 +0530

    Reduced the code from 32 lines to 15 lines
    Optimised the code by reducing the time complexity and avoiding the use of multiple functions.

--- Array_max_min.c
+++ Array_max_min.c
@@ -1,32 +1,15 @@
 #include <stdio.h>
-
-int maximum(int A[], int n){
-    int mx = A[0];
-    for (int i = 1; i < n; i++)
-    {
+int find(int A[], int n){ 
+    int mx = A[0]; int mn = A[0]; 
+    for (int i = 1; i < n; i++) { 
         if(mx < A[i]){
-            mx = A[i];
-        }
-    }
-    return mx;
-    
-}
-int minimum(int A[], int n){
-    int mn = A[0];
-    for (int i = 1; i < n; i++)
-    {
-        if(mn > A[i]){
-            mn = A[i];
-        }
-    }
-    return mn;
-    
-}
-
-int main(){
+            mx = A[i]; } 
+        if(mn > A[i]){ 
+            mn = A[i]; } } 
+    return (mx+mn); } 
+int main(){ 
     int A[] = {1, 3, 4, 1}; 
-    int size = (sizeof(A)/sizeof(int));
-    //printing the sum
-    printf("%d",minimum(A, size) + maximum(A, size) );
-    return 0;
+    int size = (sizeof(A)/sizeof(int)); 
+    printf("%d",find(A,size));
+    return 0; 
 }
