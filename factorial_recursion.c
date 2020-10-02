bf654cefc641e8b7fa1fe515d13aba9e05b57ead
thekodeway <61931801+thekodeway@users.noreply.github.com>
2020-10-02 09:51:17 +0530

    Reduced the code from 20 lines to 13 lines
    Fixed the base recursion condition for program

--- factorial_recursion.c
+++ factorial_recursion.c
@@ -1,20 +1,13 @@
 #include <stdio.h> 
-
 int factorial(int num) {
-
-  if(num <= 1) {
-    return 1;
-  }
-  
+  if(num == 0) {
+    return 1; }
   return num * factorial(num-1);
 }
-
 int main() { 
-  
   int num, result;
   printf("Insert the number: ");
   scanf("%d", &num);
-  result = factorial(num);
-  printf("Result = %d", result);
+  printf("Result = %d", factorial(num));
 	return 0; 
 }
